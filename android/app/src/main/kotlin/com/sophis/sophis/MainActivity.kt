389de94abf26261d18447ea1fe388006df830baf
DerMoha <46383545+DerMoha@users.noreply.github.com>
2026-03-24 07:15:18 +0100

    feat: add Health Connect permissions and switch to FlutterFragmentActivity

--- android/app/src/main/kotlin/com/sophis/sophis/MainActivity.kt
+++ android/app/src/main/kotlin/com/sophis/sophis/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.sophis.sophis
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
