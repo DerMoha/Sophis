6af5783f3c4e427f3f55ae251235a6219bef4fdc
DerMoha <46383545+DerMoha@users.noreply.github.com>
2026-04-16 19:10:01 +0200

    chore: remove unused legacy stores directory

--- lib/stores/stores.dart
+++
@@ -1,7 +0,0 @@
-export 'food_store.dart';
-export 'water_store.dart';
-export 'workout_store.dart';
-export 'weight_store.dart';
-export 'recipe_store.dart';
-export 'meal_plan_store.dart';
-export 'stats_store.dart';
